0c3d5e72558218bab12bf68ea5e85bb532c35ead
Ivan Prignano <ivanprignano@gmail.com>
2025-11-11 10:52:52 +0100

    add save/load/share modals to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,29 +1,28 @@
 import { createSignal } from 'solid-js';
 import SaveSongModal from '../SaveSongModal/SaveSongModal';
 import LoadSongModal from '../LoadSongModal/LoadSongModal';
+import ShareSongModal from '../ShareSongModal/ShareSongModal';
 import styles from './styles.module.css';
 
 export default function Header() {
   const [isSavingSong, setIsSavingSong] = createSignal(false);
   const [isLoadingSong, setIsLoadingSong] = createSignal(false);
-
-  const onSaveClick = () => {
-    setIsSavingSong(true);
-  };
-  const onLoadClick = () => {
-    setIsLoadingSong(true);
-  };
+  const [isSharingSong, setIsSharingSong] = createSignal(false);
 
   return (
     <header class={styles.header}>
-      <button class={styles.button} type="button" onClick={onSaveClick}>
+      <button class={styles.button} type="button" onClick={() => setIsSavingSong(true)}>
         Save
       </button>
-      <button class={styles.button} type="button" onClick={onLoadClick}>
+      <button class={styles.button} type="button" onClick={() => setIsLoadingSong(true)}>
         Load
+      </button>
+      <button class={styles.button} type="button" onClick={() => setIsSharingSong(true)}>
+        Share
       </button>
       {isSavingSong() && <SaveSongModal onClose={() => setIsSavingSong(false)} />}
       {isLoadingSong() && <LoadSongModal onClose={() => setIsLoadingSong(false)} />}
+      {isSharingSong() && <ShareSongModal onClose={() => setIsSharingSong(false)} />}
     </header>
   );
 }
