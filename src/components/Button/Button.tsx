18fca52228bfc4310ed6514e341c555e21a24a27
Ivan Prignano <ivanprignano@gmail.com>
2025-11-18 15:12:47 +0100

    fix Button component props reactivity

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,25 +2,25 @@ import { Dynamic } from 'solid-js/web';
 import { Load } from '../Icon/Load';
 import { Save } from '../Icon/Save';
 import { Copy } from '../Icon/Copy';
+import { Check } from '../Icon/Check';
 import styles from './styles.module.css';
 import type { ComponentProps, ValidComponent } from 'solid-js';
 
-// TODO: Add icons
 const iconComponents: Record<string, ValidComponent> = {
   load: () => <Load />,
   save: () => <Save />,
   copy: () => <Copy />,
+  check: () => <Check />,
 };
 
 export default function Button(
   props: {
     icon?: string;
-    variant?: 'primary' | 'alternate' | 'red';
+    variant?: 'primary' | 'alternate' | 'red' | 'green';
   } & ComponentProps<'button'>,
 ) {
-  const variant = props.variant ?? 'primary';
   return (
-    <button class={`${styles.button} ${styles[variant]}`} {...props}>
+    <button class={`${styles.button} ${styles[props.variant ?? 'primary']}`} {...props}>
       {props.icon && <Dynamic component={iconComponents[props.icon]} />}
       {props.children}
     </button>
