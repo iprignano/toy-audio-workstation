ede867980dec3f71321f423fd555d86d6c5f49cc
Ivan Prignano <ivanprignano@gmail.com>
2025-12-12 13:42:49 +0100

    add HelpModal to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,20 @@
-import { createSignal } from 'solid-js';
+import { createSignal, lazy } from 'solid-js';
 import SaveSongModal from '../SaveSongModal/SaveSongModal';
 import LoadSongModal from '../LoadSongModal/LoadSongModal';
 import ShareSongModal from '../ShareSongModal/ShareSongModal';
-import styles from './styles.module.css';
 import { Save } from '../Icon/Save';
 import { Load } from '../Icon/Load';
 import { Share } from '../Icon/Share';
+import { Help } from '../Icon/Help';
+import styles from './styles.module.css';
+
+const HelpModal = lazy(() => import('../HelpModal/HelpModal'));
 
 export default function Header() {
   const [isSavingSong, setIsSavingSong] = createSignal(false);
   const [isLoadingSong, setIsLoadingSong] = createSignal(false);
   const [isSharingSong, setIsSharingSong] = createSignal(false);
+  const [isHelpOpen, setIsHelpOpen] = createSignal(false);
 
   return (
     <header class={styles.header}>
@@ -31,10 +35,15 @@ export default function Header() {
           <Share />
           <span>Share</span>
         </button>
+        <button class={styles.button} type="button" onClick={() => setIsHelpOpen(true)}>
+          <Help />
+          <span>Help</span>
+        </button>
       </div>
       {isSavingSong() && <SaveSongModal onClose={() => setIsSavingSong(false)} />}
       {isLoadingSong() && <LoadSongModal onClose={() => setIsLoadingSong(false)} />}
       {isSharingSong() && <ShareSongModal onClose={() => setIsSharingSong(false)} />}
+      {isHelpOpen() && <HelpModal onClose={() => setIsHelpOpen(false)} />}
     </header>
   );
 }
